2e519efd071b5cc3cad7376dec1a5077cbcc5550
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-05-30 14:51:06 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Person} from './model/person.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sandbox';
+  title = 'Ma sandbox'
+  name = 'Thomas Lhomme'
+  textColor = 'green'
+  hiddenTitle = false
+  count = 0;
+  person: Person = {
+    age: 37,
+    firstname: 'Thomas',
+    lastname:'Lhomme'
+  }
+
+  increaseCount() {
+    this.count++
+  }
+
 }
---
+++ src/app/model/person.model.ts
@@ -0,0 +1,5 @@
+export interface Person{
+  firstname: string,
+  lastname: string,
+  age: number
+}
