117517393e796ca5fa2a335145b12562c4d14f6a
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-06-01 10:48:04 +0300

    rjxs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Person} from './model/person.model';
 import {NameService} from './name.service';
+import {BehaviorSubject, filter, interval, map, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,20 @@ import {NameService} from './name.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
+
+
+
+
+
+
+
   title = 'Ma sandbox'
   name = 'Thomas Lhomme'
   textColor = 'green'
   hiddenTitle = false
   count = 0;
+
   person: Person = {
     age: 37,
     firstname: 'Thomas',
@@ -26,7 +36,23 @@ export class AppComponent implements OnInit{
   constructor(private nameService: NameService) {}
 
   ngOnInit() {
+    interval(1000)
+      .pipe(
+        filter(v => v % 10 == 0),
+        map(v => v * 5)
+      ).subscribe(v => console.log(v))
+
+    let s:BehaviorSubject<string> = new BehaviorSubject('val');
+
+    s.subscribe(v => console.log(v))
+    s.next('Toto')
+    s.next('Toto')
+    s.next('Toto')
+
+
     this.names = this.nameService.names
+
+
   }
 
   increaseCount() {
